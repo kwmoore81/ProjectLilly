aedec6f2aefef5c1d3c87304f40bb1b5ad476d6c
kwmoore81 <kwmoore81@gmail.com>
2017-05-05 22:09:05 -0700

    Added Corruption Wall Destruction

---
+++ Assets/_Scripts/GravityToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityToggle : MonoBehaviour {
+
+    public GameObject overWorldMaster;
+    private OverworldSceneChanger1 SC1;
+    public float corrutionThreshold = 15;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        SC1 = overWorldMaster.GetComponent<OverworldSceneChanger1>();
+        rb = this.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (SC1.currentAreaCorruption <= corrutionThreshold)
+        {
+            rb.useGravity = true;
+        }
+	}
+}
