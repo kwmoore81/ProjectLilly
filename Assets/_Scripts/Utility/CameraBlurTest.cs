fd9af11f85817be9488f5cf26f3fd3de4deed6d3
kwmoore81 <kwmoore81@gmail.com>
2017-05-18 17:54:03 -0700

    Transition Crashes
    By LIVE

--- Assets/_Scripts/Utility/CameraBlurTest.cs
+++ Assets/_Scripts/Utility/CameraBlurTest.cs
@@ -6,68 +6,79 @@ using UnityEngine.UI;
 public class CameraBlurTest : MonoBehaviour {
 
     //public GameObject backGround;
-    public Image image;
-    public float lerpSpeed = 1.0f;
-    public float maxAlpha = 1.0f;
-    public float minAlpha = 0.0f;
-    float currentAlpha = 1.0f;
+    private Image image;
+    public float lerpSpeed = 1.0f;   
+    public float currentAlpha = 1.0f;
+    public float RotationSpeed = 10.0f;
+
+    public GameObject thirdPersonController;
+    Animator animator;
+    Rigidbody rb;
+
+    public GameObject SC1;
+    private OverworldSceneChanger1 overWorldSceaneChanger1;
+
+    public GameObject thirdPersonCamera;
+    private v3rdPersonCamera vthirdPersonCamera;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        image = GetComponent<Image>();
-        //currentAlpha = image.color.a;
+        image = GetComponentInChildren<Image>();
+        animator = thirdPersonController.GetComponent<Animator>();
+        rb = thirdPersonController.GetComponent<Rigidbody>();
+        overWorldSceaneChanger1 = SC1.GetComponent<OverworldSceneChanger1>();
+        vthirdPersonCamera = thirdPersonCamera.GetComponent<v3rdPersonCamera>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
-       
+        image.transform.Rotate(Vector3.back * (RotationSpeed * Time.deltaTime));    
 	if (Input.GetKeyDown(KeyCode.M))
         {        
-            StartCoroutine(FadeIn(currentAlpha, lerpSpeed));       
-        }
-    //else if (Input.GetKeyDown(KeyCode.N))
-    //    {
-    //        StartCoroutine(FadeOut(1.0f, 0.0f));
-    //    }	
+            StartCoroutine(FadeIn(currentAlpha, lerpSpeed));          
+        }    
 	}
 
-    IEnumerator FadeIn(float currentAlpha, float lerpSpeed)
+    public IEnumerator FadeIn(float currentAlpha, float lerpSpeed)
     {
-        Color NewAlpha = new Color(255, 255, 255, 0);
-        float Alpha = NewAlpha.a;
+        image.color = new Color(255, 255, 255, 0);
+        float Alpha = image.color.a;
 
-     
-            for (float i = 0.0f; i < 1.0f; i += Time.deltaTime / lerpSpeed)
-            {
-                Color newColor = new Color(255, 255, 255, Mathf.Lerp(Alpha, currentAlpha, i));
-                image.color = newColor;
+        animator.enabled = false;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        vthirdPersonCamera.lockCamera = true;
 
-                yield return null;
-            if (i >= 1.0f)
-            {
-                for (float j = 0.0f; i < 1.0f; j += Time.deltaTime / lerpSpeed)
-                {
-                    Color newColorFade = new Color(255, 255, 255, Mathf.Lerp(Alpha, currentAlpha - 1.0f, j));
-                    image.color = newColorFade;
+        for (float i = 0.0f; i < 1.0f; i += Time.deltaTime / lerpSpeed)
+        {
+            Color newColor = new Color(255, 255, 255, Mathf.Lerp(Alpha, currentAlpha, i));
+            image.color = newColor;
 
-                    yield return null;
-                }
-            }
-
+            yield return null;
         }
-    }
-          
 
-    //IEnumerator FadeOut(float amount, float Duration)
-    //{
-    //    //Color newColor = new Color(255, 255, 255, 100);
-    //    //image.color = newColor;
-    //    float alpha = image.color.a;
-    //    for (float i = 1.0f; i > 0.0f; i -= Time.deltaTime / Duration)
-    //    {
-            
-    //    }
-    //    yield return null;
-    //}
+        animator.enabled = true;
+       
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        vthirdPersonCamera.lockCamera = false;
+        
+        yield return new WaitForSeconds(0.3f);
+
+        overWorldSceaneChanger1.DelayedSceenChange();
+
+        Alpha = image.color.a;
+
+        for (float j = 0.0f; j < 1.0f; j += Time.deltaTime / 1.0f)
+        {
+            Color newColor = new Color(255, 255, 255, Mathf.Lerp(Alpha, currentAlpha - 1.0f, j));
+            image.color = newColor;
+
+            yield return null;
+        }
+
+        image.color = new Color(255, 255, 255, 0);
+        yield return null;
+    }
+  
 }
