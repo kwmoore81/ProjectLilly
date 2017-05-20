91a2141980dc7b175a432faba75b9854e4c52d8c
kwmoore81 <kwmoore81@gmail.com>
2017-05-19 17:26:33 -0700

    Fixed Ground Fog

---
+++ Assets/_Scripts/UI/UpdateCorruptionOW.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCorruptionOW : MonoBehaviour {
+
+    public Image corruption_Bar;
+
+    public GameObject OverworldMaster;
+    private OverworldSceneChanger1 SC1;
+    public float currentCorruption;
+    private float maxCorruption = 500;
+    private float oldCorruption;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitializeCorruptionMeter();       
+        SC1 = OverworldMaster.GetComponent<OverworldSceneChanger1>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateCorruption();
+	}
+
+    void updateCorruption()
+    {
+        currentCorruption = SC1.currentAreaCorruption;
+       
+        if (oldCorruption != currentCorruption)
+        {
+            InitializeCorruptionMeter();
+        }
+    }
+    private void InitializeCorruptionMeter()
+    {
+        float corruption_FillPercentage = currentCorruption / maxCorruption;
+        corruption_Bar.transform.localScale = new Vector3(Mathf.Clamp(corruption_FillPercentage, 0, 1),
+                         corruption_Bar.transform.localScale.y, corruption_Bar.transform.localScale.z);
+        oldCorruption = currentCorruption;
+    }
+
+}
